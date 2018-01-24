078f6c9f7b22a9213522cbe7ebb61aedf22f6222
Parker Hewitt <Hewittparker@gmail.com>
2018-01-24 14:51:24 -0700

    fixed some small bugs

---
+++ src/app/components/app-navbar/app-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+@Component({
+  selector: "app-app-navbar",
+  templateUrl: "./app-navbar.component.html",
+  styleUrls: ["./app-navbar.component.css"]
+})
+export class AppNavbarComponent implements OnInit {
+  constructor(public auth: AuthService) {}
+
+  ngOnInit() {}
+}
